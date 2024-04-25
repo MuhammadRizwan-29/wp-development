c2aaccad80eaf645dc7d3b735dab4ffb2f6d7e2b
MuhammadRizwan-29 <engrrizwanjamali@gmail.com>
2024-04-25 18:18:06 +0500

    First Plugin has been created. Display at sidebar of admin menu & also created a custom post type for the Event. Also Event Single Post file has been created in the theme named as single-event.php' did not match any files.

---
+++ wp-content/plugins/wpt-plugin/notes.md
@@ -0,0 +1,3 @@
+### WordPress Folder Structure
+- How to create folder sturture in wordpress
+- Here the souce https://www.techtic.com/blog/wordpress-theme-development-basics/
--- wp-content/plugins/wpt-plugin/wpt-plugin.php
+++ wp-content/plugins/wpt-plugin/wpt-plugin.php
@@ -11,4 +11,76 @@
     * Text Domain:       This is developed for only learning purpose. 
     * Domain Path:       /languages 
     */
+
+    // --> Direct access restricted
+    if( !defined('ABSPATH') ):
+        die("can't access");
+    endif;
+
+    /* Activation Hook */
+    function wpt_plugin_activate() {
+        // Code
+        $user = wp_get_current_user();
+        $welcome_msg = sprintf("Hello %s! Thank you for activating our plugin.", $user->user_login);
+        set_transient( 'wpt_activate_notice', $welcome_msg , 5 );
+    }
+    register_activation_hook(__FILE__, 'wpt_plugin_activate');
+
+    /** Displaying Notice at ADMIN Level */ 
+    function wpt_activate_notice_display(){
+        $activation_notice = get_transient( 'wpt_activate_notice' );
+        if($activation_notice){
+            ?>
+            <div class="notice notice-success is-dismissible">
+                <p> <?php echo esc_html($activation_notice);?> </p>
+            </div>
+            <?php
+            delete_transient( 'wpt_activate_notice' );
+        }
+    }
+    add_action('admin_notices', 'wpt_activate_notice_display');
+
+    /* De-activation Hook */
+    function wpt_plugin_deactivate(){
+        // Code
+    }
+    register_deactivation_hook( __FILE__, 'wpt_plugin_deactivate' );
+
+    /** Show setting in Administration Menu */
+
+    function wpt_show_admin_menu(){
+
+    }
+    
+    function wpt_menu_option(){
+        add_menu_page( 'WPT_PLUGIN_SETTING', 
+                        'WPT PLUGIN', 
+                        'manage_options', 
+                        'wpt-plugin-settings', 
+                        'wpt_show_admin_menu', 
+                        'dashicons-plugins-checked', 
+                        5 
+        );
+    }
+
+    add_action('admin_menu', 'wpt_menu_option');
+
+    /** Register New Post Type */ 
+
+    function wpt_event_pt(){
+        register_post_type('event', array(
+            'public' => true,
+            'supports' => array('title', 'editor', 'excerpt'),
+            'labels' => array(
+                'name' => 'Events',
+                'add_new' => 'Add New Event', 
+                'add_new_item' => 'Add New Event', 
+                'all_items' => 'All Events',
+                'edit_items' => 'Edit Event',
+            ),
+            'menu_icon' => 'dashicons-embed-video',
+        ));
+    }
+
+    add_action('init', 'wpt_event_pt');
 ?>
