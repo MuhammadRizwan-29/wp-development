b88cf350f7e46a147d8db77f37b093efaa66ebbe
MuhammadRizwan-29 <engrrizwanjamali@gmail.com>
2024-05-03 15:44:00 +0500

    WPMR Plugin initial files has been created. Direct access blocked

---
+++ wp-content/plugins/wpmr-plugin/wpmr-plugin.php
@@ -0,0 +1,15 @@
+<?php 
+    /**
+     * Plugin Name: WPMR Plugin
+     * Description: Content is the king
+     * Version: 1.0.0
+     * Author: Muhammad Rizwan
+     * Author URI: www.braincrop.net
+     * */ 
+
+/** Direct Access Forbidden */ 
+if(!defined('ABSPATH')){
+    die ('You have no access.');
+}
+
+?>
