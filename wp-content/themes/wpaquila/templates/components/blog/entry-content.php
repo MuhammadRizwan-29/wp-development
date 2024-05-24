131d8ca19916b23efd813cebda4cbc1766fef2e4
MuhammadRizwan-29 <engrrizwanjamali@gmail.com>
2024-05-24 11:35:46 +0500

    Display the POST tilte, posted-date, author-name, & excerpt.

--- wp-content/themes/wpaquila/templates/components/blog/entry-content.php
+++ wp-content/themes/wpaquila/templates/components/blog/entry-content.php
@@ -4,5 +4,25 @@
  * 
  * @package wp-aquila
  * */
-
+?>
+<div class="entry-content">
+    <?php 
+        if( is_single() ){
+            the_content(
+                sprintf(
+                    wp_kses(
+                        __('Continue reading %s <span class="meta-nav">&rarr;</span>', 'aquila'),
+                        [
+                            'span' => [
+                                'class' => []
+                            ]
+                        ]
+                    ), the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                )
+            );
+        } else {
+            aquila_the_excerpt();
+        }
+    ?>
+</div>
  
--- wp-content/themes/wpaquila/templates/components/blog/entry-meta.php
+++ wp-content/themes/wpaquila/templates/components/blog/entry-meta.php
@@ -5,4 +5,11 @@
  * @package wp-aquila
  * */
 
- 
+ ?>
+
+ <div class="entry-meta mb-3">
+    <?php 
+      aquila_posted_on(); 
+      aquila_posted_by();
+    ?>
+ </div>
